05f0a84763b2494e06a63e105d913f7b1a1d8ba6
ReaksaChan <chanreaksa.chhim@mjqeducation.edu.kh>
2025-12-17 17:36:12 +0700

    feat: configure get detail article by id route

--- routes/api/v1.php
+++ routes/api/v1.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ArticleController;
 use App\Http\Controllers\TechVerse\HomePageController;
 use Illuminate\Support\Facades\Route;
 
@@ -7,3 +8,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/homepage/services', [HomePageController::class, 'getServices']);
 Route::get('/homepage/blogs', [HomePageController::class, 'getBlogs']);
 Route::get('/homepage/case-studies', [HomePageController::class, 'getCaseStudies']);
+Route::get('/articles/{id}', [ArticleController::class, 'show']);
