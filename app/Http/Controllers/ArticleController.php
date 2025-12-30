db67c6929a1575f029317c5016d3a4163ee62c4a
ReaksaChan <chanreaksa.chhim@mjqeducation.edu.kh>
2025-12-30 11:19:25 +0700

    clean article code

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Models\Articles;
 use App\Models\CategoryArticle;
-use App\Models\Slide;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
