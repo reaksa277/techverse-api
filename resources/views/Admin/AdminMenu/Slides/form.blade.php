be56a917276b585174505b8066bbbaf47d154b7b
ReaksaChan <chanreaksa.chhim@mjqeducation.edu.kh>
2025-12-30 14:32:40 +0700

    refactor: carousel style form

--- resources/views/Admin/AdminMenu/Slides/form.blade.php
+++ resources/views/Admin/AdminMenu/Slides/form.blade.php
@@ -34,127 +34,123 @@
                         <div class="card-body">
                             <form id="slideForm" enctype="multipart/form-data">
                                 @csrf
-                                <ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
-                                    <li class="nav-item" role="presentation">
-                                        <button class="nav-link active" id="pills-home-tab" data-bs-toggle="pill"
-                                            data-bs-target="#pills-home" type="button" role="tab"
-                                            aria-controls="pills-home" aria-selected="true">English</button>
-                                    </li>
-                                    <li class="nav-item" role="presentation">
-                                        <button class="nav-link" id="pills-profile-tab" data-bs-toggle="pill"
-                                            data-bs-target="#pills-profile" type="button" role="tab"
-                                            aria-controls="pills-profile" aria-selected="false">Khmer</button>
-                                    </li>
-                                </ul>
-                                <div class="tab-content" id="pills-tabContent">
-                                    <div class="tab-pane fade show active" id="pills-home" role="tabpanel"
-                                        aria-labelledby="pills-home-tab" tabindex="0">
-                                        <div class="card">
-                                            <div class="card-body">
-                                                <div class="language_en" id="language_en">
-                                                    <div class="form-group">
-                                                        <label class="form-label">Title-en <span
-                                                                class="text-danger">*</span></label>
-                                                        <input type="text" class="form-control" id="title_en"
-                                                            value="{{ old('title_en', $data['title_en'] ?? '') }}"
-                                                            name="title_en" placeholder="Enter Title-en" required>
+                                <div class="row">
+                                    <div class="col-md-8">
+                                        <ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
+                                            <li class="nav-item" role="presentation">
+                                                <button class="nav-link active" id="pills-home-tab" data-bs-toggle="pill"
+                                                    data-bs-target="#pills-home" type="button" role="tab"
+                                                    aria-controls="pills-home" aria-selected="true">English</button>
+                                            </li>
+                                            <li class="nav-item" role="presentation">
+                                                <button class="nav-link" id="pills-profile-tab" data-bs-toggle="pill"
+                                                    data-bs-target="#pills-profile" type="button" role="tab"
+                                                    aria-controls="pills-profile" aria-selected="false">Khmer</button>
+                                            </li>
+                                        </ul>
+                                        <div class="tab-content" id="pills-tabContent">
+                                            <div class="tab-pane fade show active" id="pills-home" role="tabpanel"
+                                                aria-labelledby="pills-home-tab" tabindex="0">
+                                                <div class="card">
+                                                    <div class="card-body">
+                                                        <div class="language_en" id="language_en">
+                                                            <div class="form-group">
+                                                                <label class="form-label">Title-en <span
+                                                                        class="text-danger">*</span></label>
+                                                                <input type="text" class="form-control" id="title_en"
+                                                                    value="{{ old('title_en', $data['title_en'] ?? '') }}"
+                                                                    name="title_en" placeholder="Enter Title-en" required>
+                                                            </div>
+                                                            <div class="form-group">
+                                                                <label class="form-label"
+                                                                    for="descriptionEn">Description-en</label>
+                                                                <textarea id="description_en" name="description_en" class="summernote">{{ old('description_en', $data['description_en'] ?? '') }}</textarea>
+                                                            </div>
+                                                        </div>
                                                     </div>
-                                                    <div class="form-group">
-                                                        <label class="form-label" for="descriptionEn">Description-en</label>
-                                                        <textarea id="description_en" name="description_en" class="summernote">{{ old('description_en', $data['description_en'] ?? '') }}</textarea>
+                                                </div>
+                                            </div>
+                                            <div class="tab-pane fade" id="pills-profile" role="tabpanel"
+                                                aria-labelledby="pills-profile-tab" tabindex="0">
+                                                <div class="card">
+                                                    <div class="card-body">
+                                                        <div class="language_kh" id="language_kh">
+                                                            <div class="form-group">
+                                                                <label class="form-label">Title-kh <span
+                                                                        class="text-danger">*</span></label>
+                                                                <input type="text" class="form-control" id="title_kh"
+                                                                    value="{{ old('title_kh', $data['title_kh'] ?? '') }}"
+                                                                    name="title_kh" placeholder="Enter Title-kh" required>
+                                                            </div>
+                                                            <div class="form-group">
+                                                                <label class="form-label"
+                                                                    for="descriptionEn">Description-kh</label>
+                                                                <textarea id="description_kh" name="description_kh" class="summernote">{{ old('description_kh', $data['description_kh'] ?? '') }}</textarea>
+                                                            </div>
+                                                        </div>
+
                                                     </div>
                                                 </div>
                                             </div>
                                         </div>
                                     </div>
-                                    <div class="tab-pane fade" id="pills-profile" role="tabpanel"
-                                        aria-labelledby="pills-profile-tab" tabindex="0">
+                                    <div class="col-md-4">
                                         <div class="card">
                                             <div class="card-body">
-                                                <div class="language_kh" id="language_kh">
-                                                    <div class="form-group">
-                                                        <label class="form-label">Title-kh <span
-                                                                class="text-danger">*</span></label>
-                                                        <input type="text" class="form-control" id="title_kh"
-                                                            value="{{ old('title_kh', $data['title_kh'] ?? '') }}"
-                                                            name="title_kh" placeholder="Enter Title-kh" required>
-                                                    </div>
-                                                    <div class="form-group">
-                                                        <label class="form-label" for="descriptionEn">Description-kh</label>
-                                                        <textarea id="description_kh" name="description_kh" class="summernote">{{ old('description_kh', $data['description_kh'] ?? '') }}</textarea>
-                                                    </div>
+                                                <div class="form-group">
+                                                    <label class="form-label">Type</label>
+                                                    <select class="form-select" id="type" name="type">
+                                                        <option value="carousel"
+                                                            {{ old('type', $data['type'] ?? '') == 'carousel' ? 'selected' : '' }}>
+                                                            Carousel</option>
+                                                        <option value="advertisement"
+                                                            {{ old('type', $data['type'] ?? '') == 'advertisement' ? 'selected' : '' }}>
+                                                            Advertisement</option>
+                                                    </select>
                                                 </div>
-
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="row">
-                                        <div class="col-md-6">
-                                            <div class="card">
-                                                <div class="card-body">
-                                                    <div class="form-group">
-                                                        <label class="form-label">Type</label>
-                                                        <select class="form-select" id="type" name="type">
-                                                            <option value="carousel"
-                                                                {{ old('type', $data['type'] ?? '') == 'carousel' ? 'selected' : '' }}>
-                                                                Carousel</option>
-                                                            <option value="advertisement"
-                                                                {{ old('type', $data['type'] ?? '') == 'advertisement' ? 'selected' : '' }}>
-                                                                Advertisement</option>
-                                                        </select>
-                                                    </div>
-                                                    <div class="form-group">
-                                                        <label class="form-label">Url</label>
-                                                        <input type="text" class="form-control" id="url"
-                                                            value="{{ old('url', $data['url'] ?? '') }}" name="url"
-                                                            placeholder="Enter url">
-                                                    </div>
-                                                    <div class="form-check form-switch d-flex align-items-center p-0">
-                                                        <label class="form-check-label h5 pe-3 mb-0"
-                                                            for="status">Active</label>
-                                                        <input
-                                                            class="form-check-input h4 m-0 position-relative flex-shrink-0"
-                                                            type="checkbox" id="status" name="status" value="1"
-                                                            {{ old('status', $data['status'] ?? 0) ? 'checked' : '' }}>
-                                                    </div>
-
+                                                <div class="form-group">
+                                                    <label class="form-label">Url</label>
+                                                    <input type="text" class="form-control" id="url"
+                                                        value="{{ old('url', $data['url'] ?? '') }}" name="url"
+                                                        placeholder="Enter url">
                                                 </div>
-                                            </div>
-                                        </div>
-                                        <div class="col-md-6">
-                                            <div class="card">
-                                                <div class="card-body">
-                                                    <div class="form-group">
-                                                        <p><span class="text-danger">*</span> Carousel Picture</p>
+                                                <div class="form-group">
+                                                    <p><span class="text-danger">*</span> Article Picture</p>
 
-                                                        <!-- Custom upload button -->
-                                                        <label class="btn btn-outline-secondary" for="flupld">
-                                                            <i class="ti ti-upload me-2"></i> Click to Upload
-                                                        </label>
+                                                    <!-- Custom upload button -->
+                                                    <label class="btn btn-outline-secondary" for="flupld">
+                                                        <i class="ti ti-upload me-2"></i> Click to Upload
+                                                    </label>
 
-                                                        <!-- Hidden file input -->
-                                                        <input type="file" id="flupld" name="image"
-                                                            accept="image/*" class="d-none" onchange="loadFile(event)">
+                                                    <!-- Hidden file input -->
+                                                    <input type="file" id="flupld" name="image" accept="image/*"
+                                                        class="d-none" onchange="loadFile(event)">
 
-                                                        <!-- Preview image -->
-                                                        <img id="showThumbnail" style="margin-top:15px;max-height:100px;"
-                                                            src="{{ old('image', isset($data['image']) ? asset('storage/' . $data['image']) : '') }}">
-                                                    </div>
+                                                    <!-- Preview image -->
+                                                    <img id="showThumbnail" style="margin-top:15px;max-height:100px;"
+                                                        src="{{ old('image', isset($data['image']) ? asset('storage/' . $data['image']) : '') }}">
+                                                </div>
+                                                <div class="form-check form-switch d-flex align-items-center p-0 my-2">
+                                                    <label class="form-check-label h5 pe-3 mb-0"
+                                                        for="status">Active</label>
+                                                    <input class="form-check-input h4 m-0 position-relative flex-shrink-0"
+                                                        type="checkbox" id="status" name="status"
+                                                        {{ old('status', $data['status'] ?? 0) ? 'checked' : '' }}>
                                                 </div>
                                             </div>
                                         </div>
                                     </div>
                                     <!-- Card Footer with buttons -->
                                     <div class="text-end">
-                                        <a href="{{ route('admin.slides') }}"
+                                        <a href="{{ route('admin.categoryarticles') }}"
                                             class="btn btn-outline-secondary">Cancel</a>
-                                        <button id="btnSave" type="submit" onclick="save()"
+                                        <button id="btnSave" type="button" onclick="save()"
                                             class="btn btn-primary {{ isset($data['id']) && $data['id'] ? 'd-none' : '' }}">
-                                            Save
+                                            Save Category
                                         </button>
-                                        <button id="btnUpdate" type="submit" onclick="update({{ $data['id'] }})"
+                                        <button id="btnUpdate" type="button" onclick="update({{ $data['id'] }})"
                                             class="btn btn-primary {{ isset($data['id']) && $data['id'] ? '' : 'd-none' }}">
-                                            Update
+                                            Update Category
                                         </button>
                                     </div>
                                 </div>
